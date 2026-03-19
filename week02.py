ddd7f1e2ef846eb2648a6460fad8db815335da20
waternflame <naoireireze@gmail.com>
2026-03-19 14:38:04 +0900

    Week03 v0.4

--- week02.py
+++ week02.py
@@ -1,4 +1,4 @@
-import statistics
+import statistics   # 통계모듈 (각종 통계함수를 제공), 파이썬으로 제작된 내장 모듈로 느리다
 scores = [100, 97, 88, 91]
 average = statistics.mean(scores)
 print(average)
---
+++ week03.py
@@ -0,0 +1,4 @@
+import numpy   # 써드파티 라이브러리, C 배열 기반으로 대규모 데이터 처리에 적합. 매우 빠름
+scores = [100, 97, 88, 91]
+average = numpy.mean(scores)
+print(average)
