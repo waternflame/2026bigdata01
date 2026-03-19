b9744dd548636bcf13c4b989809b40cef20c5a36
waternflame <naoireireze@gmail.com>
2026-03-19 15:11:56 +0900

    week03 v0.6

--- week03.py
+++ week03.py
@@ -1,4 +1,4 @@
-import numpy as np   # 써드파티 라이브러리, C 배열 기반으로 대규모 데이터 처리에 적합. 매우 빠름
+import pandas as pd
 scores = [100, 97, 88, 91]
-average = np.mean(scores)
+average = pd.Series(scores).mean()
 print(average)
