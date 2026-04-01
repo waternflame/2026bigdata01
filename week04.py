6cb543824709d4c2e532b3677631ad67836690fc
waternflame <naoireireze@gmail.com>
2026-04-01 15:48:35 +0900

    week04 v0.10

--- week04.py
+++ week04.py
@@ -1,5 +1,4 @@
 import pandas as pd
 scores = [100, 97, 88, 91]
-average = pd.Series(scores).max()
+average = pd.Series(scores).min()
 print(average)
-# git push -f origin main 은 주의가 필요함(원격 리포지토리를 로컬 리포지토리로 덮어씀)
