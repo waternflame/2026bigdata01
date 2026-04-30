b5906bce23c1a7f83b4ef33a90e867b78e2cfa6e
waternflame <naoireireze@gmail.com>
2026-04-30 16:26:35 +0900

    week09 pandas v0.6

--- week09.py
+++ week09.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 df = pd.DataFrame({'국':[1, 6, 7], '영':[2, 4, 8], '수':[3, 5, 9], '화':[10, 3, 11]}, index=[1, 2, 3])
 print(df)
-df_new = df.loc[df['국']>5, ['영','화']]
-# df_new = df.loc[:,'영':'화']
-# df_new = df.iloc[:,[1, 3]]
-print(df_new)
+print(df.iat[1, 2]) #정확한 좌표값을 추출
+print(df.at[2, '수']) #라벨 기준
+print(df.sample(frac=0.33)) #frac으로 준 퍼센트 만큼 값 추출, 여기선 3개 중에 1개를 추출함
+print(df.nlargest(2, columns='화')) #가장 점수가 높은 2명을 추출, n=인원수
+print(df.nsmallest(2, columns='화')) #가장 점수가 낮은 2명을 추출
+print(df.tail(2)) #값에 대해서 상관없이 밑에서 2명 추출
