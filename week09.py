850def6f2e5aaabaceb7dc10b43d24651ac030dd
waternflame <naoireireze@gmail.com>
2026-04-30 15:39:15 +0900

    week09 pandas v0.3

--- week09.py
+++ week09.py
@@ -1,10 +1,14 @@
-import numpy as np
 import pandas as pd
 
-df = pd.DataFrame({'국':[1, 6, 7], '영':[2, 4, 8], '수':[3, 5, 9]}, index=[1, 2, 3])
+# df = pd.DataFrame({'국':[1, 6, 7], '영':[2, 4, 8], '수':[3, 5, 9]}, index=[1, 2, 3])
+# print(df)
+# # df_new = pd.melt(df)
+# # df_new = pd.melt(df).rename(columns={'variable': 'var', 'value': 'val'})
+# # df_new = pd.melt(df).rename(columns={'variable': 'var', 'value': 'val'}).query('val > 5')
+# df_new = pd.melt(df).rename(columns={'variable': 'var', 'value': 'val'}).query('val > 5').sort_values('val', ascending=False)
+# print(df_new)
+
+df = pd.DataFrame({'국':[1, 6, 7], '영':[2, 4, 8], '수':[3, 5, 9], '미세먼지':[10, 3, 11]}, index=[1, 2, 3])
 print(df)
-#df_new = pd.melt(df)
-#df_new = pd.melt(df).rename(columns={'variable': 'var', 'value': 'val'})
-df_new = pd.melt(df).rename(columns={'variable': 'var', 'value': 'val'}).query('val > 5')
-#8번줄 코드는 val 부분이 5보다 큰 경우만 출력함
+df_new = df.drop(columns=['수', '미세먼지'])
 print(df_new)
