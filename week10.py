4917109530c25cffd6b84f5c126d96ed57954dda
waternflame <naoireireze@gmail.com>
2026-05-07 15:15:51 +0900

    week10 pandas v0.9

--- week10.py
+++ week10.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
-def square(x):
-    return x*x
-
 df = pd.DataFrame({'국':[7, 6, 7], '영':[2, 4, 8], '수':[3, 5, 9], '화':[10, 3, 11]}, index=[1, 2, 3])
 print(df)
-print(df.apply(square))
+print(df.apply(lambda n: n*n))
