c7c8eaa1e657115e3c4d6f92d19b3bda1e4ce922
waternflame <naoireireze@gmail.com>
2026-05-08 10:40:27 +0900

    week10 pandas v1.1

--- week10.py
+++ week10.py
@@ -1,13 +1,12 @@
 import seaborn as sns
 
 mpg = sns.load_dataset('mpg')
-# print(mpg.head())
-# print(mpg.info())
-# print(mpg.describe())
-# print(mpg['cylinders'].value_counts())
-print(mpg[mpg['horsepower'].isnull()])
+mpg = mpg.drop(columns=['origin', 'model_year', 'name'])
+print(mpg.info())
+print(mpg.head())
+# print(mpg.sort_values('mpg', ascending=False))
 mpg['horsepower'] = mpg['horsepower'].fillna(
     mpg.groupby('cylinders')['horsepower'].transform('median')
 )
 print(mpg.info())
-print(mpg[mpg['horsepower'].isnull()])
+# print(mpg[mpg['horsepower'].isnull()])
