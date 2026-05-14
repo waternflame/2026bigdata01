3e5e52ca34997f1e4ca0a365525869ce39c3ae4c
waternflame <naoireireze@gmail.com>
2026-05-14 16:33:48 +0900

    week11 pandas v1.4

--- week11.py
+++ week11.py
@@ -1,12 +1,11 @@
 import seaborn as sns
 
 ex = sns.load_dataset('exercise')
-# print(ex.head())
-# print(ex.info())
-# print(ex['diet'].value_counts())
-# print(ex['diet'].value_counts())
-# print(len(ex[ex['diet'] == 'low fat']))
-# print(ex[ex['diet'] == 'no fat'])
-running_df = ex[ex['kind'] == 'running']
-# print(running_df)
-# sns.catplot(running_df, x='time', y='pulse', hue='diet', kind='point')
+ex = ex[
+    (ex['diet'] == 'low fat') &
+    (ex['time'] == '30 min') &
+    (ex['kind'] == 'running')
+]
+print(ex)
+mean_pulse = ex['pulse'].mean()
+print(mean_pulse)   # 5명의 평균 심박수 출력
