040be0b8b2b6b0e745e70652b3d1ef54909922d6
waternflame <naoireireze@gmail.com>
2026-05-14 16:22:49 +0900

    week11 pandas v1.3

--- week11.py
+++ week11.py
@@ -5,3 +5,8 @@ ex = sns.load_dataset('exercise')
 # print(ex.info())
 # print(ex['diet'].value_counts())
 # print(ex['diet'].value_counts())
+# print(len(ex[ex['diet'] == 'low fat']))
+# print(ex[ex['diet'] == 'no fat'])
+running_df = ex[ex['kind'] == 'running']
+# print(running_df)
+# sns.catplot(running_df, x='time', y='pulse', hue='diet', kind='point')
